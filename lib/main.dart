a60090a19441e79f94eb9fe8bee5c92323feb461
maiphuc <maidinhphucpk@gmail.com>
2024-05-06 09:18:55 +0700

    change 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -55,18 +56,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // int _counter = 0;
+  //
+  // void _incrementCounter() {
+  //   setState(() {
+  //     // This call to setState tells the Flutter framework that something has
+  //     // changed in this State, which causes it to rerun the build method below
+  //     // so that the display can reflect the updated values. If we changed
+  //     // _counter without calling setState(), then the build method would not be
+  //     // called again, and so nothing would appear to happen.
+  //     _counter++;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -86,55 +87,87 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Expanded(
-                child: Row(
-              children: [
-                ...List.generate(
-                    3,
-                    (index) => Container(
-                          color: index % 3 == 0
-                              ? Colors.orangeAccent
-                              : index % 3 == 1
-                                  ? Colors.greenAccent
-                                  : Colors.blueAccent,
-                          child: Text('$index'),
-                        ))
-              ],
-            ))
-          ],
-        ),
+      // body: Center(
+      //   // Center is a layout widget. It takes a single child and positions it
+      //   // in the middle of the parent.
+      //   child: Column(
+      //     // Column is also a layout widget. It takes a list of children and
+      //     // arranges them vertically. By default, it sizes itself to fit its
+      //     // children horizontally, and tries to be as tall as its parent.
+      //     //
+      //     // Column has various properties to control how it sizes itself and
+      //     // how it positions its children. Here we use mainAxisAlignment to
+      //     // center the children vertically; the main axis here is the vertical
+      //     // axis because Columns are vertical (the cross axis would be
+      //     // horizontal).
+      //     //
+      //     // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+      //     // action in the IDE, or press "p" in the console), to see the
+      //     // wireframe for each widget.
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       const Text(
+      //         'You have pushed the button this many times:',
+      //       ),
+      //       Text(
+      //         '$_counter',
+      //         style: Theme.of(context).textTheme.headlineMedium,
+      //       ),
+      //       Expanded(
+      //           child: Row(
+      //         children: [
+      //           ...List.generate(
+      //               3,
+      //               (index) => Container(
+      //                     color: index % 3 == 0
+      //                         ? Colors.orangeAccent
+      //                         : index % 3 == 1
+      //                             ? Colors.greenAccent
+      //                             : Colors.blueAccent,
+      //                     child: Text('$index'),
+      //                   ))
+      //         ],
+      //       ))
+      //     ],
+      //   ),
+      // ),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            color: Colors.green,
+          ),
+          Expanded(
+              child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // ...List.generate(24, (index) => Container()),
+              Flexible(flex: 24, child: Container()),
+              Column(
+                children: [
+                  Container(
+                    height: 50,
+                    color: Colors.orangeAccent,
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.red,
+                  )
+                ],
+              ),
+              Flexible(flex: 32, child: Container()),
+              // ...List.generate(32, (index) => Container()),
+            ],
+          ))
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
